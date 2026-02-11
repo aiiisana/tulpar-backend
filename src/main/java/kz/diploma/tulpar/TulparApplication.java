24442d45d2d586d494289f08fc9fdb2da60831db
aiiisana <aisanaond@gmail.com>
2026-02-11 20:58:07 +0500

    init

---
+++ src/main/java/kz/diploma/tulpar/TulparApplication.java
@@ -0,0 +1,13 @@
+package kz.diploma.tulpar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulparApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TulparApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/diploma/tulpar/TulparApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.diploma.tulpar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulparApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
